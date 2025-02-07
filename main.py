33f9613aa6d18ef5c8b877d32c666a586498d82c
Anteneh Mulu <anteneh.mulu1921@gmail.com>
2025-02-07 22:54:34 +0300

    Importing pandas

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import csv
-with open("weather_data.csv") as data:
-    read_csv=csv.reader(data)
-    temperatures=[]
-    for row in read_csv:
-        if row[1]!="temp":
-            temperatures.append(int(row[1]))
-    print(temperatures)
+# import csv
+# with open("weather_data.csv") as data:
+#     read_csv=csv.reader(data)
+#     temperatures=[]
+#     for row in read_csv:
+#         if row[1]!="temp":
+#             temperatures.append(int(row[1]))
+#     print(temperatures)
+import pandas
+data=pandas.read_csv("weather_data.csv")
+print(data["temp"])
