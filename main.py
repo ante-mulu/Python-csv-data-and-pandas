0c9064023f049c96f028fee204895c06fef44483
Anteneh Mulu <anteneh.mulu1921@gmail.com>
2025-02-14 16:06:15 +0300

    US STATE GUESS

--- main.py
+++ main.py
@@ -22,14 +22,55 @@
 # # print(average)
 #
 # # print(list_data)
+# import pandas
+# data=pandas.read_csv('Squirrel_Data.csv')
+# grey_squirrel_count=len(data[data["Primary Fur Color"] == "Gray"])
+# red_squirrel_count=len(data[data["Primary Fur Color"] == "Cinnamon"])
+# black_squirrel_count=len(data[data["Primary Fur Color"] == "Black"])
+# data_count={
+#     "Fur Color":["Gray","Cinnamon","Black"],
+#     "Count":[grey_squirrel_count,black_squirrel_count,black_squirrel_count]
+# }
+# data_frame=pandas.DataFrame(data_count)
+# data_frame.to_csv("squirrel_count.csv")
+import turtle
 import pandas
-data=pandas.read_csv('Squirrel_Data.csv')
-grey_squirrel_count=len(data[data["Primary Fur Color"] == "Gray"])
-red_squirrel_count=len(data[data["Primary Fur Color"] == "Cinnamon"])
-black_squirrel_count=len(data[data["Primary Fur Color"] == "Black"])
-data_count={
-    "Fur Color":["Gray","Cinnamon","Black"],
-    "Count":[grey_squirrel_count,black_squirrel_count,black_squirrel_count]
-}
-data_frame=pandas.DataFrame(data_count)
-data_frame.to_csv("squirrel_count.csv")
+
+screen = turtle.Screen()
+screen.title("US States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+turtle.setup(500, 500)
+
+correct_guess = []
+missing_state = []  # Initialize the list to store missing states
+data = pandas.read_csv("50_states.csv")
+list_data = data["state"].to_list()
+
+while True:
+    answer_state = screen.textinput(title=f"{len(correct_guess)}/{len(list_data)} States Correct", prompt="What is another State's name?").title()
+    if answer_state == "Exit":
+        # Collect missing states
+        for state in list_data:
+            if state not in correct_guess:
+                missing_state.append(state)
+        missing_state_data = pandas.DataFrame(missing_state, columns=["States"])
+        missing_state_data.to_csv("states_to_learn.csv", index=False)
+        break
+    if answer_state in list_data:
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(state_data.x.item(), state_data.y.item())
+        correct_guess.append(answer_state)
+        t.write(answer_state)
+    else:
+        print("no")
+    print(correct_guess)
+# def get_mouse_click_coor(x,y):
+#     print(x,y)
+# turtle.onscreenclick(get_mouse_click_coor)
+# turtle.mainloop()
+# screen.exitonclick()
